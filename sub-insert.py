db6bdffa211d6ecdcff5ba2a9af329f4b5415ce6
김성필(Sungpil Kim) <phirippa@naver.com>
2023-01-01 11:31:51 +0900

    Update sub-insert.py

--- sub-insert.py
+++ sub-insert.py
@@ -5,7 +5,7 @@ iclient = InfluxDBClient(host='localhost', port=8086,
 
 def on_connect(client, userdata, flags, rc):
     print('Connect with result code ' + str(rc))
-    mclient.subscribe('MyOffice/#')
+    client.subscribe('MyOffice/#')
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))   #msg.topic - 'MyOffice/Indoor/SensorValue'
@@ -26,9 +26,9 @@ def on_message(client, userdata, msg):
     json_body.append(data_point)
     iclient.write_points(json_body)
 
-mclient = mqtt.Client()
-mclient.username_pw_set(username='mqtt_ship', password='1234')
-mclient.on_connect = on_connect
-mclient.on_message = on_message
-mclient.connect('localhost', 1883, 60)
-mclient.loop_forever()
+client = mqtt.Client()
+client.username_pw_set(username='mqtt_ship', password='1234')
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect('localhost', 1883, 60)
+client.loop_forever()
