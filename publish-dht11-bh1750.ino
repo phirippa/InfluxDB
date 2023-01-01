871ffe98faf8a7e5c648e695e91c13d7ba667411
김성필(Sungpil Kim) <phirippa@naver.com>
2023-01-01 11:19:07 +0900

    Create publish-dht11-bh1750.ino

---
+++ publish-dht11-bh1750.ino
@@ -0,0 +1,72 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "DHT.h"
+#include <Wire.h>
+#include <BH1750.h>
+#define DHTPIN D4     
+#define DHTTYPE DHT11
+#define PAYLOADSIZE 128
+
+DHT dht(DHTPIN, DHTTYPE);
+BH1750 lightMeter;
+
+const char* ssid = "Ria2G";
+const char* password = "12345679";
+const char* userId = "mqtt_ship";
+const char* clientId = userId;
+const char* userPw = "1234";
+char* topic = "MyOffice/Indoor/SensorValue";
+char* server = "192.168.0.18"; 
+
+WiFiClient wifiClient; 
+PubSubClient client(server, 1883, wifiClient);
+
+void setup() {
+  Serial.begin(9600);
+ 
+  Serial.print("\nConnecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("\nWiFi Connected");
+  
+  Serial.println("Connecting to broker");
+  while ( !client.connect(clientId, userId, userPw) ){ 
+    Serial.print("*");
+    delay(1000);
+  }
+  Serial.println("\nConnected to broker!!");
+  dht.begin();
+  Wire.begin();
+  lightMeter.begin();
+}
+
+void loop() {
+  char payload[PAYLOADSIZE];
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float lux = lightMeter.readLightLevel();
+
+  if (isnan(h) || isnan(t) ) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  String sPayload = "{'Temp':"
+                +String(t, 1)	// 소수점 한 자리만 붙여서 생성
+                +",'Humi':"
+                +String(h, 1)
+                +",'Lux':"
+                +String(lux,1)                
+                +"}";           
+  // {'Temp':29.6, 'Humi':30.0, 'Lux':400}
+
+  sPayload.toCharArray(payload, PAYLOADSIZE);
+  client.publish(topic, payload);
+  Serial.print(String(topic) + " ");  Serial.println(payload);
+  delay(2000);
+}
