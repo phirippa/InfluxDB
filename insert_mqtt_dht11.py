642585158d7ead57e59dc9bb317e7301fb205065
김성필(Sungpil Kim) <phirippa@naver.com>
2023-01-01 11:34:14 +0900

    Update insert_mqtt_dht11.py

--- insert_mqtt_dht11.py
+++ insert_mqtt_dht11.py
@@ -4,7 +4,7 @@ iclient = InfluxDBClient(host='localhost', port=8086, username='influx_ship', pa
 
 def on_connect( client, userdata, flags, rc ):
    print('Connect with result code ' + str(rc) )
-   mclient.subscribe('MyOffice/#')
+   client.subscribe('MyOffice/#')
 
 def on_message( client, userdata, msg ):
    print( msg.topic +' '+str(msg.payload) )        # 수신되는 topic은 MyOffice/Indoor/Temp 또는 MyOffice/Indoor/Humi
@@ -20,9 +20,9 @@ def on_message( client, userdata, msg ):
    json_body.append(data_point)
    iclient.write_points( json_body )
 
-mclient = mqtt.Client( )
-mclient.username_pw_set(username='mqtt_ship', password='1234')
-mclient.on_connect = on_connect
-mclient.on_message = on_message
-mclient.connect('localhost', 1883, 60)
-mclient.loop_forever( )
+client = mqtt.Client( )
+client.username_pw_set(username='mqtt_ship', password='1234')
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect('localhost', 1883, 60)
+client.loop_forever( )
